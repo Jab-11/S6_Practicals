d0c6c0e7abbe03c4567604bad1680888e9230ad5
Jabarson Christian <115206504+Jab-11@users.noreply.github.com>
2024-02-01 14:11:52 +0000

    commit

---
+++ WP/P1/3-Clg/main.js
@@ -0,0 +1,8 @@
+let icon = document.querySelector(".bx")
+
+function tgl(){
+    icon.classList.toggle("bx-x")
+    icon.classList.toggle("bx-menu")
+    let opsm = document.querySelector(".opsm")
+    opsm
+}
